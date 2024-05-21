1c3b920b9be8b071cde25ed0f75ed1445b46ad5a
Sára Hölclová <sara.holclova@gmail.com>
2024-05-21 18:09:07 +0200

    komunikace od rodiči k díteti

--- src/components/Rate/index.jsx
+++ src/components/Rate/index.jsx
@@ -3,15 +3,17 @@ import './style.css';
 const currencies = {
   USD: { CZK: 23.823 },
   EUR: { CZK: 24.74 },
-  GBP: { CZK: 29.067 }
+  GBP: { CZK: 29.067 },
 };
 
-export const Rate = () => {
+export const Rate = ({ from }) => {
+  const rate = currencies[from].CZK;
+
   return (
     <div className="rate">
-      <div className="rate__currency">1 USD</div>
+      <div className="rate__currency">1 {from}</div>
       <div>=</div>
-      <div className="rate__value">22 CZK</div>
+      <div className="rate__value">{rate} CZK</div>
     </div>
   );
 };
